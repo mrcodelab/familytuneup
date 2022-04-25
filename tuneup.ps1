260683271111f35002f232a9932df0c58fa05cc2
mrcodelab <96273662+mrcodelab@users.noreply.github.com>
2022-04-25 14:09:08 -0500

    Homegym updates

--- tuneup.ps1
+++ tuneup.ps1
@@ -36,6 +36,9 @@ function cleanup {
 
 
 function common {
+    Write-Host "Updating the maintenance and security files"
+    Invoke-WebRequest -Uri https://github.com/schwastedotter/homegym/blob/main/zAdmin/tuneup.ps1 -OutFile 'C:\Program Files\zAdmin\tuneup.ps1'
+    #Invoke-WebRequest -Uri https://github.com/schwastedotter/homegym/blob/main/zAdmin/hosts -OutFile 'C:\Windows\System32\drivers\etc\hosts'
     Write-Host "Updating Windows"
     updater
     Write-Host "Cleaning up the system bloat"
