197148d0638703963a0bb00e8dfcfd791fc1c8f9
mrcodelab <96273662+mrcodelab@users.noreply.github.com>
2022-06-24 08:03:40 -0500

    Update tuneup.ps1

--- tuneup.ps1
+++ tuneup.ps1
@@ -1,61 +1,136 @@
-﻿$u=$env:UserName
-#$c=$env:COMPUTERNAME
+﻿#version yyyy.mm.MAJ.MIN.r
+#version 2022.06.2.1.1
+$u=$env:UserName
+$c=$env:COMPUTERNAME
 Write-Output "Hi $u. "
-#$b="C:\Program Files\zAdmin"
+$b="C:\Program Files\zAdmin"
+$h = "C:\Windows\System32\drivers\etc\hosts"
 $mo=Get-Date -UFormat %b
 $sched = "Jan", "Mar", "May", "Jul", "Sep", "Nov"
 $task = Read-Host "Do you need a reboot (r) || shutdown(s) || keep awake(k)"
+$task = $task.ToUpper()
 
-function stateTogg {    
-    if ( $task -eq "s" ) {
-        Stop-Computer
-        } 
-    elseif ($task -eq 'r') {
-        Restart-Computer
+function msdtTest {
+    try {
+        New-PSDrive -PSProvider registry -Root HKEY_CLASSES_ROOT -Name HKCR
+    }
+    catch {
+        -ErrorAction Ignore
+    }
+
+    Set-Location -Path HKCR:\
+    $keyname = "HKCR:\ms-msdt"
+    if ( Get-ChildItem -Path $keyname -ErrorAction Ignore ) {
+        Write-Host "it exists" -ForegroundColor Red
+        Remove-Item -Recurse -Force $keyname -WhatIf
+        Write-Host "MSDT is removed." -ForegroundColor Green
+    }
+    else {
+        write-host "MSDT is already gone." -ForegroundColor Green
+    }
+}
+
+function stateTogg {
+    if ( $task -eq 'S' ) {
+        Stop-Computer -Force
+        }
+    elseif ($task -eq 'R') {
+        Restart-Computer -Force
         }
     else { Write-Host "Keeping awake"}
 }
 
-function updater {
-    Get-WUInstall -Install -AcceptAll -AutoReboot
-    Get-WUInstall -MicrosoftUpdate -Install -AcceptAll -AutoReboot
+function gitUpdater{
+    try {
+        Write-Host "Updating the maintenance and security files" -ForegroundColor Yellow
+        Invoke-WebRequest -Uri https://raw.githubusercontent.com/mrcodelab/familytuneup/main/tuneup.ps1 -OutFile '$HOME\Downloads'
+        $zip1 = Get-FileHash -Algorithm SHA256 $HOME\Downloads\w10_pc_tuneup.ps1 | Select-Object -ExpandProperty Hash
+        Invoke-WebRequest -Uri https://raw.githubusercontent.com/mrcodelab/familytuneup/main/tuneup-hash.txt -OutFile '$HOME\Downloads'
+        $hash1 = Get-Content $HOME\Downloads\w10_pc_hash.txt
+        if ( $zip1 -eq $hash1 ) {
+            Move-Item w10_pc_tuneup.ps1 $b
+            Write-Host "Tuneup file updated." -ForegroundColor Green
+        }
+        else { Write-Host "The tuneup hash did not match!" -ForegroundColor Red }
     }
+    catch {
+        -ErrorAction SilentlyContinue
+    }
+
+    try {
+        Invoke-WebRequest -Uri https://raw.githubusercontent.com/mrcodelab/pihole-g/main/hosts -OutFile '$HOME\Downloads'
+        $zip2 = Get-FileHash -Algorithm SHA256 $HOME\Downloads\hosts | Select-Object -ExpandProperty Hash
+        Invoke-WebRequest -Uri https://raw.githubusercontent.com/mrcodelab/pihole-g/main/hosts_hash.txt -OutFile '$HOME\Downloads'
+        $hash2 = Get-Content $HOME\Downloads\hosts_hash.txt
+        if ( $zip2 -eq $hash2 ) {
+            Move-Item $Home\Downloads\hosts $h
+            Write-Host "Hosts file updated." -ForegroundColor Green
+        }
+        else { Write-Host "The host hash did not match!" -ForegroundColor Red }
+    }
+    catch {
+        -ErrorAction SilentlyContinue
+    }
+    Write-Host "Security file update blocked by antivirus. No biggie." -ForegroundColor White
+
+}
+
+function updater {
+    Get-WUInstall -Install -AcceptAll -AutoReboot -Hide
+    Get-WUInstall -MicrosoftUpdate -Install -AcceptAll -AutoReboot -Hide
+}
+
+function dldclnr {
+    if( $c -ne "MightyMouse"){
+        Remove-Item -Path $Home\Downloads\* -Recurse -Force
+        Write-Host "Done cleaning downloads" -ForegroundColor Green
+    }
+    else { 
+        Remove-Item $HOME\Downloads\*.gz -Recurse
+        Write-Host "Not deleting downloads folder" 
+    }
+}
 
 function cleanup {
     Set-Location $HOME
-    Clear-RecycleBin -Force
-    Write-Host "Recycle Bin cleaned - Ignore the error. It works."
-    Set-Location $HOME\Downloads
-    Remove-Item -Recurse *
-    Remove-Item c:\Windows\Temp\* -Recurse -Force -ErrorAction SilentlyContinue
-    $PSScriptRoot
+    Clear-RecycleBin -DriveLetter C -Force -ErrorAction SilentlyContinue
+    Write-Host "Recycle Bin cleaned - Ignore the error. It works." -ForegroundColor Green
+    dldclnr
+    Write-Host "Cleaning the temp folder." -ForegroundColor Yellow
+    Remove-Item -Path C:\Windows\Temp\* -Recurse -Force -ErrorAction SilentlyContinue
+    Write-Host "Temp folder cleaned." -ForegroundColor Green
+    Set-Location $PSScriptRoot
     Start-Process -FilePath "C:\WINDOWS\system32\cleanmgr.exe" /sagerun:1 | Out-Null
     Wait-Process -Name cleanmgr
     Start-Process -FilePath "C:\Program Files (x86)\Wise\Wise Registry Cleaner\WiseRegCleaner.exe"
-    }
+}
 
 
 function common {
-    Write-Host "Updating the maintenance and security files"
-    Invoke-WebRequest https://github.com/mrcodelab/ServerAdminTinkering/blob/main/Windows/tuneup.ps1 -OutFile 'C:\Program Files\zAdmin\tuneup.ps1'
-    Invoke-WebRequest https://github.com/mrcodelab/ServerAdminTinkering/blob/main/Windows/hosts -OutFile 'C:\Windows\System32\drivers\etc\hosts'
-    Write-Host "Updating Windows"
+    Write-Host "Testing MSDT vulnerability" -ForegroundColor Yellow
+    msdtTest
+    Set-Location $b
+    Write-Host "Updating Windows" -ForegroundColor Yellow
     updater
-    Write-Host "Cleaning up the system bloat"
+    gitUpdater
+    Write-Host "Windows update complete." -ForegroundColor Green
+    Write-Host "Cleaning up the system bloat" -ForegroundColor Yellow
     cleanup
+    Write-Host "Disk cleanup complete." - -ForegroundColor Green
     stateTogg
     Set-Location $HOME
 }
 
-Write-Output "Running common workload"
+Write-Host "Running common workload" -ForegroundColor Yellow
+
 common
 
 $PSScriptRoot
 
 if (( $u -eq "mateusz" ) -and ( $mo -in $sched )) {
-    Write-Output "running admin special"
+    Write-Host "running admin special" -ForegroundColor Yellow
     & "$PSScriptRoot\pwmaintenance.ps1"
-    } else { Write-Output "Not now Madeline" }
+    } else { Write-Host "Not now Madeline" -ForegroundColor Blue}
 
 
 Stop-Process -Name powershell
